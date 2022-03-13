39eac63ef964865de959fddf715d93a3b1269896
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-13 22:25:01 +0300

    added a templates folder as well as set up my models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
+class Award(models.Model):
+    name=models.CharField(max_length=300)
+    description=models.TextField(max_length=5000)     
+    developer=models.CharField(max_length=300)
+    created_date=models.DateField()
+    averangeRating=models.FloatField()
+
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
