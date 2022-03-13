7a587be58ee379c61baa6cff4e69b96387f15a95
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-13 22:38:24 +0300

    testing out linking of my pages

--- main/views.py
+++ main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+
 # Create your views here.
+# main logic
+def home(request):
+    # accessing all details from the database created on models files
+    allAwardsDetails=Award.objects.all()
+
+    context={"awards":allAwardsDetails}
+    return render (request, 'main/index.html', context)
