3b69a3397281bb69471d431185b2901f4bf5567b
LeonDante-ctrl <maxgamerdu@gmail.com>
2022-03-25 11:56:09 +0300

    incorperated search references

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import *
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+# creating the logic here for accounts registration
+def register(request):
+    if request.method=="POST":
+        form =RegistrationForm(request.POST or None)
+
+
+        # check if the form is valid
+        if form.is_valid():
+            user=form.save()
+
+
+            # get the password
+            gettpassword=form.cleaned_data.get('password1')
+            # authenticating the user
+            user=authenticate(username=user.username, password=gettpassword)
+            # login the user now
+            login(request, user)
+            return redirect("accounts:login")
+        else:
+            form=RegistrationForm()
+    return render(request,"accounts/register.html", {"form":form})
+
+# login function
+def login_user(request):
+    if request.method =="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+
+        # check the credential
+        user=authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("main:home")
+            else:
+                return render(request, "accounts/login.html",{"error":"Your account has been disabled."})
+        else:
+
+            return render(request,"accounts/login.html",{"error":"Invalid Username or Password. Try Again or Check Your credentials."})   
+    return render(request, "accounts/login.html")         
+
+def logout_user(request):
+    logout(request)
+    return redirect("accounts:login")        
